57bd5d2edb4946e60d7e83bd5195fd00a97be0ac
Michael Mandl <mandlm@molez.net>
2018-10-29 19:52:51 +0100

    Display a random number

--- slider.py
+++ slider.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("hello.html")
+    return render_template("hello.html", num = randint(1, 23))
 
 if __name__ == "__main__":
     app.run()
