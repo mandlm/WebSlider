96993ac3d0dadc91b3e9125b6e5bab7107c38e5e
Michael Mandl <mandlm@molez.net>
2018-11-03 14:00:06 +0100

    Serve a random image from imgfolder on /random_image endpoint

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-imgdir = "/home/mandlm/Foto Export/2018/09/2018-09-03"
+imgdir = "."
--- slider.py
+++ slider.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for
 from random import randint
+from os import listdir
+import random
 
 import config
 
@@ -11,6 +13,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("hello.html", num=randint(1, 23))
+
+
+@app.route("/random_image")
+def random_image():
+    filename = random.choice(listdir(config.imgdir))
+    return redirect(url_for("image", filename=filename))
 
 
 @app.route("/img/<path:filename>")
