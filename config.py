f0fb5f0d9fa7e06e49f8c46d590fd3bc355c3c3d
Michael Mandl <mandlm@molez.net>
2018-11-03 13:21:26 +0100

    Serve images from a separate direcory configured in an external config file (config.py)

---
+++ config.py
@@ -0,0 +1,1 @@
+imgdir = "/home/mandlm/Foto Export/2018/09/2018-09-03"
--- slider.py
+++ slider.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from random import randint
+
+import config
 
 app = Flask(__name__)
 
@@ -9,6 +11,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("hello.html", num=randint(1, 23))
+
+
+@app.route("/img/<path:filename>")
+def image(filename):
+    return send_from_directory(config.imgdir, filename)
 
 
 if __name__ == "__main__":
