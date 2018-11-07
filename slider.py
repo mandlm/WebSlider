41d7c104afd328679136b6d8a9e513a8a7ce9788
Michael Mandl <mandlm@molez.net>
2018-11-08 00:10:08 +0100

    Prefer recent images

--- slider.py
+++ slider.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, send_from_directory, redirect, url_for
 from random import choice
 from pathlib import Path
 from PIL import Image
+from time import time
 
 import config
 
@@ -18,16 +19,26 @@ def random():
 @app.route("/random_image/")
 def random_image():
     imgdir = Path(config.imgdir)
-    images = list(imgdir.glob("*.jpg"))
-    selected_image = choice(images).relative_to(imgdir)
+    last_modified_time, last_modified_file = max(
+        (f.stat().st_mtime, f) for f in imgdir.glob("*.jpg")
+    )
+
+    if time() - last_modified_time <= 60:
+        selected_image = last_modified_file.relative_to(imgdir)
+    else:
+        images = list(imgdir.glob("*.jpg"))
+        selected_image = choice(images).relative_to(imgdir)
+
     return redirect(url_for("image", filename=selected_image))
 
 
 @app.route("/img/<path:filename>")
 def image(filename):
     scaled_img_dir = Path(config.imgdir) / ".slider" / "fhd"
+
     if not scaled_img_dir.exists():
         scaled_img_dir.mkdir(parents=True)
+
     if not (scaled_img_dir / filename).exists():
         img = Image.open(Path(config.imgdir) / filename)
         img.thumbnail((1920, 1080))
